422ad4db9a31c7cbb96f616172f6076e64baffee
Caio Anderson <caio.anderson@queonetics.com>
2025-10-07 14:10:23 -0300

    remove React import

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,49 +1,84 @@
-import React, { useState } from "react"
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Header.css'
 import Logo from '../assets/logo.png'
 
 const Header = () => {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false); // Estado para controlar a visibilidade da sidebar
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false) // Estado para controlar a visibilidade da sidebar
 
-    const toggleSidebar = () => {
-        setIsSidebarOpen(!isSidebarOpen);
-    };
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen)
+  }
 
-    return (
-        
-        <header className="header">
-            {/* burger button*/}
-            <button className="menu-toggle" onClick={toggleSidebar} aria-label="Abrir menu">
-                <span className="hamburger-icon"></span>
-                <span className="hamburger-icon"></span>
-                <span className="hamburger-icon"></span>
-            </button>
+  return (
+    <header className="header">
+      {/* burger button*/}
+      <button
+        className="menu-toggle"
+        onClick={toggleSidebar}
+        aria-label="Abrir menu"
+      >
+        <span className="hamburger-icon"></span>
+        <span className="hamburger-icon"></span>
+        <span className="hamburger-icon"></span>
+      </button>
 
-        <div className="header-logo">
-            <img src={Logo} alt="Logo da Competição" />
-            {/*<span className="logo-placeholder">LOGO AQUI</span>*/}
-        </div>
+      <div className="header-logo">
+        <img src={Logo} alt="Logo da Competição" />
+        {/*<span className="logo-placeholder">LOGO AQUI</span>*/}
+      </div>
 
-        <div className="header-spacer"></div>
+      <div className="header-spacer"></div>
 
       {/* Sidebar de navegação */}
       <nav className={`sidebar ${isSidebarOpen ? 'is-open' : ''}`}>
-                <ul className="sidebar-menu">
-                    <li> <Link to="/" onClick={toggleSidebar}>Início</Link></li>
-                    <li> <Link to="/jogos" onClick={toggleSidebar}>Jogos</Link></li>
-                    <li> <Link to="/equipes" onClick={toggleSidebar}>Equipes</Link></li>
-                    <li> <Link to="/mata-mata" onClick={toggleSidebar}>Mata-Mata</Link></li>
-                    <li> <Link to="/artilharia" onClick={toggleSidebar}>Artilharia</Link></li>
-                    <li> <Link to="/noticias" onClick={toggleSidebar}>Noticias</Link></li>
-                </ul>
-            </nav>
+        <ul className="sidebar-menu">
+          <li>
+            {' '}
+            <Link to="/" onClick={toggleSidebar}>
+              Início
+            </Link>
+          </li>
+          <li>
+            {' '}
+            <Link to="/jogos" onClick={toggleSidebar}>
+              Jogos
+            </Link>
+          </li>
+          <li>
+            {' '}
+            <Link to="/equipes" onClick={toggleSidebar}>
+              Equipes
+            </Link>
+          </li>
+          <li>
+            {' '}
+            <Link to="/mata-mata" onClick={toggleSidebar}>
+              Mata-Mata
+            </Link>
+          </li>
+          <li>
+            {' '}
+            <Link to="/artilharia" onClick={toggleSidebar}>
+              Artilharia
+            </Link>
+          </li>
+          <li>
+            {' '}
+            <Link to="/noticias" onClick={toggleSidebar}>
+              Noticias
+            </Link>
+          </li>
+        </ul>
+      </nav>
 
-            {/* Overlay para fechar a sidebar clicando fora dela */}
-            {isSidebarOpen && <div className="sidebar-overlay" onClick={toggleSidebar}></div>}
-            {/*<div className="header-spacer"></div>*/}
-        </header>
-    )
+      {/* Overlay para fechar a sidebar clicando fora dela */}
+      {isSidebarOpen && (
+        <div className="sidebar-overlay" onClick={toggleSidebar}></div>
+      )}
+      {/*<div className="header-spacer"></div>*/}
+    </header>
+  )
 }
 
 export default Header
