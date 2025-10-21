8fbc938cd431c0b6d0344ca81d71acacd913fc42
Caio Anderson <caio.anderson@queonetics.com>
2025-10-21 20:27:29 -0300

    feat: adding top players score data

---
+++ src/constants/top-scorers.ts
@@ -0,0 +1,6 @@
+export const topScorers = [
+  { name: 'Caio', countGols: 3, team: 'Teste'},
+  { name: 'John', countGols: 3, team: 'Teste'},
+  { name: 'Sena', countGols: 4, team: 'Teste 2'},
+  { name: 'Alek', countGols: 1, team: 'Teste 3'},
+]
